a0116abd0057f630b1b9e700c82db8f3ad62dcd2
Ivan <alfaandomega789@gmail.com>
2024-01-15 13:32:02 +0300

    init commit

--- src/main/homework/homework_35/WorkWithArray.java
+++ src/main/homework/homework_35/WorkWithArray.java
@@ -5,11 +5,21 @@ import java.util.Map;
 
 public class WorkWithArray {
     public static void main(String[] args) {
-        String[] array1 = new String[]{"a", "b", "a", "c", "b"};
+        String[] array1 = new String[]{"a", "b", "a", "c", "b"}; //task 1
         String[] array2 = new String[]{"c","b","d"};
+        String[] array3 = new String[]{"c","c","c","c"};
 
         System.out.println(wordMultiple(array1));
         System.out.println(wordMultiple(array2));
+        System.out.println(wordMultiple(array3));
+
+        String[] array4 = new String[]{"code", "bug"}; //task 2
+        String[] array5 = new String[]{"man", "moon", "main"};
+        String[] array6 = new String[]{"man", "moon", "good", "night"};
+
+        System.out.println(pairs(array4));
+        System.out.println(pairs(array5));
+        System.out.println(pairs(array6));
     }
 
     public static Map<String, Boolean> wordMultiple(String[] array){
@@ -26,7 +36,17 @@ public class WorkWithArray {
                 }
             }
         }
-
         return mp;
+    }
+
+    public static Map<String, String> pairs(String[] array){
+        Map<String, String> map = new HashMap<>();
+
+        for(String str : array){
+            char[] ch = str.toCharArray();
+            map.put(ch[0] + " ", ch[ch.length - 1] + " ");
+        }
+
+        return map;
     }
 }
